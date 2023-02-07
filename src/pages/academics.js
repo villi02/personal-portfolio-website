5990444e457e1995e8cff34766fb72ca1a87e819
villi02 <villiarnar1@gmail.com>
2023-02-07 13:26:46 +0100

    implemented NavBar positioning

--- src/pages/academics.js
+++ src/pages/academics.js
@@ -1,12 +1,14 @@
 import React from 'react';
-import { NavBar } from '../components/NavBar';
 import { Experience } from '../components/Experience';
 import colorSharp from "../assets/img/color-sharp.png"
 
 const Academics = () => {
     return (
-        <div>
+        <div id='body'>
+            
             <img className="background-image-left" src={colorSharp}/>
+
+            <h1>Academics</h1>
         </div>
             
         );
