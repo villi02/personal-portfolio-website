16eb2f308de03d008ecfcc179a40877321cb0573
villi02 <villiarnar1@gmail.com>
2023-04-03 11:26:00 +0200

    changes to academics page

--- src/components/academics/ClassCards.js
+++ src/components/academics/ClassCards.js
@@ -1,24 +1,30 @@
 import CardType  from 'react-bootstrap/Card';
 import ListGroup from 'react-bootstrap/ListGroup';
+import Button from "react-bootstrap/Button";
+import { BsGithub } from "react-icons/bs";
+import ntnuLogo from "../../assets/img/projects/ntnulogo.png"
 import React from "react";
 
 function classCards(props) {
   return (
     <CardType className="project-card-view" style={{ width: '18rem' }}>
-      <CardType.Img variant="top" src={props.imgPath} />
+      <CardType.Img variant="top" src={ntnuLogo} style={{backgroundColor:"white"}} />
       <CardType.Body>
         <CardType.Title>{props.title}</CardType.Title>
         <CardType.Text style={{ textAlign: "justify", position: "relative", paddingBottom: "20px" }}>
           {props.description}
         </CardType.Text>
       </CardType.Body>
-      <ListGroup className="list-group-flush">
-        <ListGroup.Item>{props.code}</ListGroup.Item>
+      <ListGroup>
+        <ListGroup.Item className='class-code-view'>{props.code}</ListGroup.Item>
       </ListGroup>
       <CardType.Body>
-        <CardType.Link href={props.weburl}>Course website</CardType.Link>
+        <CardType.Link style={{bottom:"10px"}} href={props.weburl}>Course website</CardType.Link>
         {!props.hasCode && props.ghLink && (
-          <CardType.Link href={props.ghLink}>Course website</CardType.Link>
+           <Button variant="primary" href={props.ghLink} target="_blank" style={{position: "absolute" , bottom:"10px", right:"10px" }}>
+           <BsGithub /> &nbsp;
+           {"GitHub"}
+         </Button>
         )}
       </CardType.Body>
     </CardType>
--- src/pages/academics.js
+++ src/pages/academics.js
@@ -15,6 +15,12 @@ const Academics = () => {
                 <p style={{ color: "white" }}>
                     Here are the courses I've taken during University
                 </p>
+                <p style={{ color: "white" }}>
+                <strong className="physics">Math&Physics</strong>
+                </p>
+                <p style={{ color: "white" }}>
+                <strong className="CS">Computer Science</strong>
+                </p>
                 <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
                     <Col md={4} className="project-card">
                         <ClassCard
@@ -23,8 +29,32 @@ const Academics = () => {
                             code="FY1003"
                             weburl="https://www.ntnu.no/studier/emner/FY1003/2022#tab=omEmnet"
                         />
-                            
-                        
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ClassCard
+                            title="Wave Physics and Fluid Mechanics"
+                            description="Introduction to wavephysics and fluidmechanicsWavephysics: Description of wave motion, harmonic waves, the wave equation, elastic waves, wave packets, wave phenomena, dispersion.Continuummechanics/fluidmechanics: The continuum hypothesis, conservation laws, pressure, the control volume method, differential methods in flow systems, Navier-Stokes' equation. Various flow system."
+                            code="TFY4163"
+                            weburl="https://www.ntnu.edu/studies/courses/TFY4163#tab=omEmnet"
+                            ghLink="https://github.com/villi02/TFY4163-Bolgefysikk-og-fluidmekanikk"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ClassCard
+                            title="Mathematics 2 - Multivariable calculus and vector analysis"
+                            description="Curves in space. Functions of several variables. Taylor's theorem in two dimensions, extremal values in several variables, Lagrange multipliers. Double and triple integrals, line and surface integrals. Vector calculus. The theorems of Green, Stokes, and Gauss."
+                            code="TMA4105"
+                            weburl="https://www.ntnu.edu/studies/courses/TMA4105#tab=omEmnet"
+                        />
+                    </Col>
+                    <Col md={4} className="project-card">
+                        <ClassCard
+                            title="Mathematics 3 - Linear algebra"
+                            description="Complex numbers and the complex exponential function. Second order linear differential equations. Systems of linear equations, Gauss-Jordan elimination, reduced echelon form, matrix algebra, determinants. Vector spaces, subspaces, linear dependence and independence. Inner products, orthogonality, orthogonal projections, Gram-Schmidt orthogonalization, and the method of least squares. Eigenvalues and eigenvectors, diagonalization, symmetric matrices and quadratic forms. First order systems of differential equations."
+                            code="TMA4115"
+                            weburl="https://www.ntnu.no/studier/emner/FY1003/2022#tab=omEmnet"
+                            ghLink="everythingforagreencard.com"
+                        />
                     </Col>
                 </Row>
             </Container>
