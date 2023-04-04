c6a5d5af99989fca2f1933ae7ad9a15ce612e5e8
villi02 <villiarnar1@gmail.com>
2023-04-04 10:04:11 +0200

    implemented support for language and framework overview to project cards

--- src/components/projectPage/ProjectCards.js
+++ src/components/projectPage/ProjectCards.js
@@ -4,6 +4,7 @@ import Card from "react-bootstrap/Card";
 import Button from "react-bootstrap/Button";
 import { CgWebsite } from "react-icons/cg";
 import { BsGithub } from "react-icons/bs";
+import ListGroup from 'react-bootstrap/ListGroup';
 
 function ProjectCards(props) {
   return (
@@ -14,6 +15,13 @@ function ProjectCards(props) {
         <Card.Text style={{ textAlign: "justify", position: "relative", paddingBottom: "20px" }}>
           {props.description}
         </Card.Text>
+        {props.language && props.frameworks && (
+          <ListGroup style={{paddingTop:"15px", paddingBottom:"10px"}}>
+            <ListGroup.Item className="project-code-lang-view" >{props.language}</ListGroup.Item>
+            <ListGroup.Item className="project-code-lang-view">{props.frameworks}</ListGroup.Item>
+          </ListGroup>
+        )}
+        
         <Button variant="primary" href={props.ghLink} target="_blank" style={{position: "absolute" , bottom:"10px" }}>
           <BsGithub /> &nbsp;
           {props.isBlog ? "Blog" : "GitHub"}
--- src/pages/projects.js
+++ src/pages/projects.js
@@ -30,17 +30,33 @@ function Projects() {
               isBlog={false}
               title="Dividend Diary"
               description="An IOS app built for tracking dividend oriented portfolio. The app is built with Swift for the frontend, firebase for backend and financialmodelingprep API for live stock data. Built as the final project for CS50x, an online course taught by Harvard University"
+              language="Swift"
+              frameworks="Firebase, SwiftUI, FinancialModelingPrep API"
               ghLink="https://github.com/villi02/CS50x/tree/main/Final%20project/DivDiary"
             />
           </Col>
+          <Col md={4} className="project-card">
+            <ProjectCard
+              imgPath={stockimg}
+              isBlog={false}
+              title="Time Series AI Stock Predictor"
+              description="A project where I served as project lead for Cogito NTNU, a student organization, revolving around using AI to predict the stock prices of the Oslo Stock Exchange. Inspired by renaissance technologies, we used out-of-marked data in combination with mathematics and advanced algorithms to predict if the stock would go up or down the following day. There were two main models for the project, one LSTM and a random forest classifier."
+              language="Python"
+              frameworks="Tensorflow, Pandas, yahoo Finance API"
+              ghLink="https://github.com/CogitoNTNU/Stockbot"
+            />
+          </Col>
+
 
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={warGamesPic}
+              imgPath={memegenimg}
               isBlog={false}
-              title="Wargames"
-              description="A war simulator game, allowing players to create armies from predefined solider classes. My solution for the project know as 'Wargames' for the subject IDATT2001 at NTNU"
-              ghLink="https://github.com/villi02/Wargames"
+              title="Meme AI"
+              description="Used an LSTM model to try to make funny one-liners. Used code/ai-model from a Stanford research project to make an application that inserted meme text onto an image to make a meme out of any picture."
+              language="Python"
+              frameworks="Tensorflow, Pandas"
+              ghLink="https://github.com/CogitoNTNU/memeAI"
             />
           </Col>
 
@@ -50,41 +66,23 @@ function Projects() {
               isBlog={false}
               title="Portfolio-website"
               description="My personal portfolio website (this website). Written in JavaScript and CSS, using React and Bootstrap."
+              language="JavaScript, CSS"
+              frameworks="React, react-bootstrap"
               ghLink="https://github.com/villi02/personal-portfolio-website"              
             />
           </Col>
-
           <Col md={4} className="project-card">
             <ProjectCard
-              imgPath={stockimg}
+              imgPath={warGamesPic}
               isBlog={false}
-              title="Time Series AI Stock Predictor"
-              description="A project where I served as project lead for Cogito NTNU, a student organization, revolving around using AI to predict the stock prices of the Oslo Stock Exchange. Inspired by renaissance technologies, we used out-of-marked data in combination with mathematics and advanced algorithms to predict if the stock would go up or down the following day. There were two main models for the project, one LSTM and a random forest classifier."
-              ghLink="https://github.com/CogitoNTNU/Stockbot"
+              title="Wargames"
+              description="A war simulator game, allowing players to create armies from predefined solider classes. My solution for the project know as 'Wargames' for the subject IDATT2001 at NTNU"
+              language="Java, CSS"
+              frameworks="javaFX"
+              ghLink="https://github.com/villi02/Wargames"
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={memegenimg}
-              isBlog={false}
-              title="Ai For Social Good"
-              description="Used an LSTM model to try to make funny one-liners. Used code/ai-model from a Stanford research project to make an application that inserted meme text onto an image to make a meme out of any picture."
-              ghLink="https://github.com/CogitoNTNU/memeAI"
-            />
-          </Col>
-
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={emotion}
-              isBlog={false}
-              title="Face Recognition and Emotion Detection"
-              description="Trained a CNN classifier using 'FER-2013 dataset' with Keras and tensorflow backened. The classifier sucessfully predicted the various types of emotions of human. And the highest accuracy obtained with the model was 60.1%.
-              Then used Open-CV to detect the face in an image and then pass the face to the classifer to predict the emotion of a person."
-              ghLink="https://github.com/soumyajit4419/Face_And_Emotion_Detection"
-              // demoLink="https://blogs.soumya-jit.tech/"      <--------Please include a demo link here 
-            />
-          </Col>
         </Row>
       </Container>
     </Container>
